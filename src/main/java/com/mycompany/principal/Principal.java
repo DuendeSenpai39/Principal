23cd62cf3c7d94cfdf4e531eca77e0a6e8b6c230
Jose <Jose@DESKTOP-G0V731S>
2022-10-13 20:37:06 -0500

    Final

---
+++ src/main/java/com/mycompany/principal/Principal.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.principal;
+
+/**
+ *
+ * @author Jose
+ */
+public class Principal {
+
+public static void main(String [] args){
+       
+        var auto1 = new Auto();
+        auto1.placa="ABF8592";
+        auto1.marca="Chevrolet";
+        auto1.nombrePropietario="Juana Moreno";
+        auto1.precio=15200;
+        auto1.color="Amarillo";
+        auto1.year=2017;
+        
+        System.out.println(auto1.mostrarInfo()+" el país de origen de la marca es "+auto1.getPaisMarca());
+        System.out.println("La provincia de matrícula es: "+auto1.getProvincia());
+        System.out.println("El auto tiene" + auto1.calcularEdad(2022) + " años");
+        System.out.println("El numero de continente es: " +auto1.getNumeroContinente ());
+        System.out.println("El costo de la matricula es: " +auto1.calcularCostoMatricula(2022));
+        
+
+        
+        var auto2 = new Auto();
+        auto2.placa="GFR0422";
+        auto2.marca="Toyota";
+        auto2.nombrePropietario="Mariana Ortiz";
+        auto2.color="Negro";
+        auto2.precio=25000;
+        auto2.year=2019;
+        
+        System.out.println(auto2.mostrarInfo()+" el país de origen de la marca es "+auto2.getPaisMarca());
+        System.out.println("La provincia de matrícula es: "+auto2.getProvincia());
+        System.out.println("El auto tiene" + auto2.calcularEdad(2022) + " años");
+        System.out.println("El numero de continente es: " +auto2.getNumeroContinente ());
+        System.out.println("El costo de la matricula es: " +auto2.calcularCostoMatricula(2022));
+        
+        
+        var auto3 = new Auto();
+        auto3.placa="UKJ0075";
+        auto3.marca="Jetour";
+        auto3.nombrePropietario="Juan Ortiz";
+        auto3.color="Rojo";
+        auto3.precio=9000;
+        auto3.year=2004;
+        
+        System.out.println(auto3.mostrarInfo()+" el país de origen de la marca es "+auto3.getPaisMarca());
+        System.out.println("La provincia de matrícula es: "+auto3.getProvincia());
+        System.out.println("El auto tiene" + auto3.calcularEdad(2022) + " años");
+        System.out.println("El numero de continente es: " +auto3.getNumeroContinente ());
+        System.out.println("El costo de la matricula es: " +auto3.calcularCostoMatricula(2022));
+        
+        
+        
+        
+    }
+    
+    
+    
+}
